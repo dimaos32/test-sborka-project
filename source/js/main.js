6f8fd518914a4486f286a90c5c8f3c92fa900044
Dima Os <dimaos@protonmail.com>
2022-10-24 16:15:43 +0300

    Prepare project

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {initCustomSelect} from './modules/form/init-custom-select';
-import {initFormValidate} from './modules/form/init-form-validate';
 
 // ---------------------------------
 
@@ -18,9 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    initCustomSelect();
-    initFormValidate();
+
   });
 });
 
